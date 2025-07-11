7af57aa494b208a46fdee0d9b921991187c35ec5
EmilianoHinojosa <myemail>
2025-07-11 00:33:35 -0600

    firebase profile query

--- constants/firebase.ts
+++ constants/firebase.ts
@@ -1,6 +1,7 @@
-import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCPEOXkzuJ9BXEOoVFpg8iKqowyar_kp6o",
@@ -17,3 +18,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
+
+export const db = getDatabase(app);
